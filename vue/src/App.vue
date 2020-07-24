e8291b39292308ab10123bdf3477cf1df948a182
waricoma <waricoma@devne.co>
2020-07-24 14:41:31 +0900

    :sparkles: add: 3D SYSTEM

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div -->
     <router-view />
   </div>
 </template>
@@ -28,5 +28,9 @@
       color: #42b983;
     }
   }
+}
+
+body {
+  margin: 0 auto;
 }
 </style>
